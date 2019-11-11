02ae6809dd1c079f58f112561083631f1a09b013
Aditya <adityaraj.mishra@sap.com>
2019-11-11 13:20:07 +0530

    Added Tm4j Files

---
+++ bank/src/main/java/com/tm4j/main/Calculator.java
@@ -0,0 +1,10 @@
+package com.tm4j.main;
+
+public class Calculator {
+
+    public int sum(int i, int j) {
+        // TODO Auto-generated method stub
+        return i + j;
+    }
+
+}
---
+++ bank/src/test/java/com/tm4j/test/CalculatorSumTest.java
@@ -0,0 +1,38 @@
+package com.tm4j.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.adaptavist.tm4j.junit.annotation.TestCase;
+import com.tm4j.main.Calculator;
+
+public class CalculatorSumTest {
+    
+    @Test
+    @TestCase(key = "JQA-T1")
+    public void sumTwoNumbersAndPass() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(5, calculator.sum(3, 2));
+    }
+ 
+    @Test
+    @TestCase(key = "JQA-T2")
+    public void sumTwoNumbersAndFail() {
+        Calculator calculator = new Calculator();
+        Assert.assertNotEquals(2, calculator.sum(1, 2));
+    }
+ 
+    @Test
+    public void notMappedToTestCaseAndPass() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(3, calculator.sum(1, 2));
+    }
+ 
+    @Test
+    @TestCase(name = "Mapped to a Test Case Name and Pass")
+    public void mappedToATestCaseNameAndPass() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(4, calculator.sum(2, 2));
+    }
+ 
+}
